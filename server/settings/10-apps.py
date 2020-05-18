ec3d331cfb8d4a1bad2d67148f4b86d5cc0f6dba
chriscauley <chris@lablackey.com>
2020-05-18 18:41:04 +0000

    initial python fles for django project

---
+++ server/settings/10-apps.py
@@ -0,0 +1,24 @@
+INSTALLED_APPS = [
+    # django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.gis',
+
+    # 3rd party
+    'sorl.thumbnail',
+
+    # this project
+    'server',
+
+    # unrest
+    'unrest',
+    'unrest.nopass',
+    'unrest.user',
+]
+
+AUTH_USER_MODEL = 'user.User'
