23a5973cfdb1cfbd935e12b282c912f2dabe80b4
chriscauley <chris@lablackey.com>
2020-12-29 13:13:39 +0000

    upgrading django version

--- todo/models.py
+++ todo/models.py
@@ -1,19 +1,18 @@
 from django.conf import settings
 from django.db import models
-from django.contrib.postgres.fields import JSONField
 from unrest.models import BaseModel, _choices
 
 
 class Action(BaseModel):
     activity = models.ForeignKey('Activity', on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    data = JSONField(default=dict)
+    data = models.JSONField(default=dict)
     __str__ = lambda self: self.activity.name
 
 class Thing(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
-    data = JSONField(default=dict)
+    data = models.JSONField(default=dict)
     __str__ = lambda self: self.name
 
 
