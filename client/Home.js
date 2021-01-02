2de584e07d0de0f39ffd077715e5debba6463e58
chriscauley <chris@lablackey.com>
2021-01-02 15:22:59 +0000

    add project now a button

--- client/Home.js
+++ client/Home.js
@@ -7,13 +7,20 @@ import css from '@unrest/css'
 export default function Home() {
   const { projects = [] } = api.project.use()
   return (
-    <div className={css.list.outer()}>
-      {projects.map(({ id, name }) => (
-        <Link to={`/project/${id}/`} key={id} className={css.list.item()}>
-          {name}
+    <div>
+      <div className={css.list.outer()}>
+        {projects.map(({ id, name }) => (
+          <Link to={`/project/${id}/`} key={id} className={css.list.item()}>
+            {name}
+          </Link>
+        ))}
+      </div>
+      <div>
+        <Link className={css.button('inline-block')} to="/project/new/">
+          <i className={css.icon('plus mr-3')} />
+          Add Project
         </Link>
-      ))}
-      <Link to="/project/new/">Add Project</Link>
+      </div>
     </div>
   )
 }
