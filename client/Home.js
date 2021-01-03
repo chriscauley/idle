a1ba649f2cd8e53ade1c705c9ee188dd52afb1f0
chriscauley <chris@lablackey.com>
2021-01-03 11:36:56 +0000

    show running task spinners on home page

--- client/Home.js
+++ client/Home.js
@@ -4,14 +4,29 @@ import { Link } from 'react-router-dom'
 import api from './task/api'
 import css from '@unrest/css'
 
+function OpenTasks({ tasks }) {
+  tasks = tasks.filter((t) => t.started && !t.completed)
+  return (
+    <div className="flex">
+      {tasks.map((task) => (
+        <div key={task.id} className="text--text-alt">
+          <i className={css.icon('spinner fa-spin mr-2')} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
 export default function Home() {
   const { projects = [] } = api.project.use()
+  const { tasks = [] } = api.task.use()
   return (
     <div>
       <div className={css.list.outer()}>
         {projects.map(({ id, name }) => (
           <Link to={`/project/${id}/`} key={id} className={css.list.item()}>
-            {name}
+            <div className="flex-grow">{name}</div>
+            <OpenTasks tasks={tasks.filter((t) => t.project_id === id)} />
           </Link>
         ))}
       </div>
