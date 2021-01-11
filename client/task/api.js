3681d17ecf2aaf216f28ac38ee613b1f27d2ced8
chriscauley <chris@lablackey.com>
2021-01-11 12:50:20 +0000

    due date was being assigned wrong

--- client/task/api.js
+++ client/task/api.js
@@ -1,7 +1,13 @@
 import RestHook from '@unrest/react-rest-hook'
+import { sortBy } from 'lodash'
+
+const prepTaskData = (data) => {
+  data.tasks = sortBy(data.tasks, 'due')
+  return data
+}
 
 export default {
   project: RestHook('/api/project/'),
   activity: RestHook('/api/activity/'),
-  task: RestHook('/api/task/'),
+  task: RestHook('/api/task/', { prepData: prepTaskData }),
 }
--- client/task/createTasks.js
+++ client/task/createTasks.js
@@ -1,10 +1,12 @@
-import { addDays } from 'date-fns'
+import { addDays, max } from 'date-fns'
+import { pick, sortBy } from 'lodash'
 import { post } from '@unrest/core'
 
 import api from './api'
 
 let loading
 
+// TODO should be an action in api
 export default () => {
   const { activities } = api.activity.use()
   const { tasks, refetch } = api.task.use()
@@ -16,17 +18,20 @@ export default () => {
     const activity_tasks = tasks.filter((t) => t.activity_id === activity.id)
     const new_task = activity_tasks.find((t) => !t.completed)
     if (!new_task) {
-      const last_task = activity_tasks.find((t) => t.completed)
+      const last_task = sortBy(activity_tasks, (t) => t.due || 0)
+        .reverse()
+        .find((t) => t.completed)
       const due = addDays(
-        last_task?.completed || new Date().valueOf(),
+        max([new Date(last_task?.completed || 0), new Date()]),
         1,
       ).valueOf()
       const { name, project_id } = last_task || activity
       const activity_id = activity.id
+      const extras = pick(last_task, activity.texts, activity.measurements)
       promises.push(
         post(`/api/schema/TaskForm/`, {
           name,
-          data: { activity_id, project_id, due },
+          data: { activity_id, project_id, due, ...extras },
         }),
       )
     }
