5328f73c7458c456504478ecfab594826f850fb8
chriscauley <chris@lablackey.com>
2021-01-12 10:53:15 +0000

    typo

--- client/app.js
+++ client/app.js
@@ -9,7 +9,7 @@ import Home from './Home'
 import Nav from './Nav'
 import ProjectForm from './task/ProjectForm'
 import ActivityForm, { CreateTaskActivity } from './task/ActivityForm'
-import { ActivityProjectRedirect } from './task/ActivityProjectRedirect'
+import { ActivityProjectRedirect } from './task/ActivityProject'
 import ProjectDetail from './task/ProjectDetail'
 
 // TODO this is where photos, activities, actions, and things can be cross associated
